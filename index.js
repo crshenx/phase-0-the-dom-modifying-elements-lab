befcf00d6dff543ea0db03eb9e29cae84d13b4b0
Chris Hendricks <crshenx@gmail.com>
2022-06-09 11:16:12 -0700

    commit tree 0e2e56fba7e3256bf8f333f1279b5d24dadaf23e

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+// const divElement = document.createElement("div");
+// document.body.append(divElement);
+
+// const ulElement = document.createElement("ul");
+// for (let i = 0; i < 100; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   li.style.color = `#2764${i}B`;
+//   ulElement.append(li);
+// }
+
+// document.body.append(ulElement);
+
+console.log(document.querySelector("h1").textContent);
+const firstHeading = document.querySelector("#firstHeading");
+firstHeading.remove();
+
+document.getElementById("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Christopwerwer is the champion";
